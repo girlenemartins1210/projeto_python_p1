119b439cdceba820a58f10f48937847df5f987de
notwiu <166825050+notwiu@users.noreply.github.com>
2024-05-06 17:13:51 -0300

    botões-numero
    Foram adicionados os botões contendo os números da calculadora

--- calculadora.py
+++ calculadora.py
@@ -42,6 +42,28 @@ b_7 = Button(frame_corpo, text="=", width= 5, height= 3, bg= cor6, font=("Ivy 13
 b_7.place(x= 177, y=200)
 
 
+#botões-numeros
+b_8 = Button(frame_corpo, text="7", width= 5, height= 2, bg= cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_8.place(x= 0, y=50)
+b_9 = Button(frame_corpo, text="8", width= 5, height= 2, bg= cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_9.place(x= 60, y=50)
+b_10 = Button(frame_corpo, text="9", width= 5, height= 2, bg= cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_10.place(x= 120, y=50)
+b_11 = Button(frame_corpo, text="4", width= 5, height= 2, bg= cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_11.place(x= 0, y=100)
+b_12 = Button(frame_corpo, text="5", width= 5, height= 2, bg= cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_12.place(x= 60, y=100)
+b_13 = Button(frame_corpo, text="6", width= 5, height= 2, bg= cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_13.place(x= 120, y=100)
+b_14 = Button(frame_corpo, text="1", width= 5, height= 2, bg= cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_14.place(x= 0, y=150)
+b_15 = Button(frame_corpo, text="2", width= 5, height= 2, bg= cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_15.place(x= 60, y=150)
+b_16 = Button(frame_corpo, text="3", width= 5, height= 2, bg= cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_16.place(x= 120, y=150)
+b_17 = Button(frame_corpo, text="0", width= 17, height= 3, bg= cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_17.place(x= 0, y=200)
+
 
 
 
